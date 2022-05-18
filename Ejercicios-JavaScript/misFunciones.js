1ebe813715d4367bc806124d3958170b1105d212
lautarocorrea1 <chipi.correa01@gmail.com>
2022-05-18 20:44:19 -0300

    Se realizo ej conversor unidades

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,40 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiounidades(id , valor) {
+    if (isNaN(valor)) {
+        alert("Se ingreso un valor invalido");
+        document.LasUnidades.unid_metro.value = "";
+        document.LasUnidades.unid_pulgada.value = "";
+        document.LasUnidades.unid_pie.value = "";
+        document.LasUnidades.unid_yarda.value = "";
+    } else if (id=="metro")
+    {
+        document.LasUnidades.unid_pulgada.value = 39.3701 * valor;
+        document.LasUnidades.unid_pie.value = 3.28084 * valor;
+        document.LasUnidades.unid_yarda.value = 1.09361 * valor;
+
+    }
+    else if (id=="pulgada")
+    {
+        document.LasUnidades.unid_metro.value = 0.0254 * valor;
+        document.LasUnidades.unid_pie.value = 0.83333 * valor;
+        document.LasUnidades.unid_yarda.value = 0.0277778 * valor;
+
+    }
+    else if (id=="pie")
+    {
+        document.LasUnidades.unid_metro.value = 3.28084*valor;
+        document.LasUnidades.unid_pulgada.value = 39.3701*valor;
+        document.LasUnidades.unid_yarda.value = 0.33333*valor;
+
+    }
+
+    else if(id=="yarda"){
+        document.LasUnidades.unid_metro.value = 3.28084*valor;
+        document.LasUnidades.unid_pulgada.value = 39.3701*valor;
+        document.LasUnidades.unid_pie.value = 0.33333*valor;
+
+    }
+
+}
