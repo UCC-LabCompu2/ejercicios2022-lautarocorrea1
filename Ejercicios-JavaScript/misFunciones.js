e66c7dc0f4ee81c579d19be40d9db64c901adc74
lautarocorrea1 <chipi.correa01@gmail.com>
2022-06-07 16:57:41 -0300

    Se realizo ejercicio grados a radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -12,33 +12,41 @@ function cambiounidades(id , valor) {
         document.LasUnidades.unid_pulgada.value = "";
         document.LasUnidades.unid_pie.value = "";
         document.LasUnidades.unid_yarda.value = "";
-    } else if (id=="metro")
-    {
+    } else if (id == "metro") {
         document.LasUnidades.unid_pulgada.value = 39.3701 * valor;
         document.LasUnidades.unid_pie.value = 3.28084 * valor;
         document.LasUnidades.unid_yarda.value = 1.09361 * valor;
 
-    }
-    else if (id=="pulgada")
-    {
+    } else if (id == "pulgada") {
         document.LasUnidades.unid_metro.value = 0.0254 * valor;
         document.LasUnidades.unid_pie.value = 0.83333 * valor;
         document.LasUnidades.unid_yarda.value = 0.0277778 * valor;
 
-    }
-    else if (id=="pie")
-    {
-        document.LasUnidades.unid_metro.value = 3.28084*valor;
-        document.LasUnidades.unid_pulgada.value = 39.3701*valor;
-        document.LasUnidades.unid_yarda.value = 0.33333*valor;
+    } else if (id == "pie") {
+        document.LasUnidades.unid_metro.value = 3.28084 * valor;
+        document.LasUnidades.unid_pulgada.value = 39.3701 * valor;
+        document.LasUnidades.unid_yarda.value = 0.33333 * valor;
+
+    } else if (id == "yarda") {
+        document.LasUnidades.unid_metro.value = 3.28084 * valor;
+        document.LasUnidades.unid_pulgada.value = 39.3701 * valor;
+        document.LasUnidades.unid_pie.value = 0.33333 * valor;
 
     }
-
-    else if(id=="yarda"){
-        document.LasUnidades.unid_metro.value = 3.28084*valor;
-        document.LasUnidades.unid_pulgada.value = 39.3701*valor;
-        document.LasUnidades.unid_pie.value = 0.33333*valor;
-
-    }
-
 }
+
+function convertirGR(id){
+    var grad, rad;
+    if(id=="grados"){
+        grad = document.getElementById(elementId: "grados").value;
+        rad = (grad*math.PI)/180;
+    } else if(id=="radianes") {
+        rad = document.getElementById (elementId: "radianes").value;
+        grad = (rad*180)/math.PI;
+    }
+    document.getElementById(elementId:"grados").value = grad;
+    document.getElementById(elementId:"radianes").value = rad;
+}
+
+
+
